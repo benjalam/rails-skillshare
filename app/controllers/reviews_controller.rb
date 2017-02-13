7bae17dc9aa6c018880b490e97bc51d0ac9420ab
Leslie Maarek <maarek.leslie@gmail.com>
2017-02-13 18:26:22 +0100

    refacto reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,16 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_booking, only: [:new, :create]
   def index
     @reviews = Review.all
   end
 
   def new
-    @booking = Booking.find(params[:booking_id])
     @review = Review.new
   end
 
   def create
   @review = Review.new(review_params)
-  @booking = Booking.find(params[:booking_id])
   @review.booking = @booking
   if @review.save
     redirect_to booking_path(@booking)
@@ -21,15 +21,12 @@ class ReviewsController < ApplicationController
   end
 
   def show
-    @review = Review.find(params[:id])
   end
 
   def edit
-    @review = Review.find(param[:id])
   end
 
   def update
-    @review = Review.find(param[:id])
     if @review.update(review_params)
        redirect_to booking_path(@booking)
     else
@@ -38,7 +35,6 @@ class ReviewsController < ApplicationController
   end
 
    def destroy
-    @review = Review.find(params[:id])
     @booking = @review.booking
     @review.destroy
     redirect_to booking_path(@booking)
@@ -50,5 +46,12 @@ class ReviewsController < ApplicationController
    params.require(:review).permit(:title, :description, :rating)
  end
 
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
+  end
 end
 
