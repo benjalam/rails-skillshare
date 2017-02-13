2ab5bf91321e4aebf27021e7a4a2d7cd98839927
Leslie Maarek <maarek.leslie@gmail.com>
2017-02-13 18:03:43 +0100

    finish reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,16 +9,39 @@ class ReviewsController < ApplicationController
   end
 
   def create
-   @review = Review.new(review_params)
-   @booking = Booking.find(params[:booking_id])
-   @review.booking = @booking
-   @review.save
-   redirect_to booking_path(@booking)
+  @review = Review.new(review_params)
+  @booking = Booking.find(params[:booking_id])
+  @review.booking = @booking
+  if @review.save
+    redirect_to booking_path(@booking)
+  else
+    render :new
+  end
    # il faut nester review dans booking
   end
 
   def show
     @review = Review.find(params[:id])
+  end
+
+  def edit
+    @review = Review.find(param[:id])
+  end
+
+  def update
+    @review = Review.find(param[:id])
+    if @review.update(review_params)
+       redirect_to booking_path(@booking)
+    else
+      render :edit
+    end
+  end
+
+   def destroy
+    @review = Review.find(params[:id])
+    @booking = @review.booking
+    @review.destroy
+    redirect_to booking_path(@booking)
   end
 
   private
