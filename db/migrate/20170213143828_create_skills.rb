83443c4ac7e961ef3356920bfe7eeb39af81fac9
Benjamin Lambrou <lambroubenjamin@gmail.com>
2017-02-13 15:46:41 +0100

    creation of reviews, skills, bookings models

---
+++ db/migrate/20170213143828_create_skills.rb
@@ -0,0 +1,12 @@
+class CreateSkills < ActiveRecord::Migration[5.0]
+  def change
+    create_table :skills do |t|
+      t.string :city
+      t.string :title
+      t.text :description
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213135253) do
+ActiveRecord::Schema.define(version: 20170213144234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.string   "start_date"
+    t.string   "end_date"
+    t.string   "status"
+    t.integer  "user_id"
+    t.integer  "skill_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["skill_id"], name: "index_bookings_on_skill_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "nature"
+    t.integer  "rating"
+    t.integer  "booking_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id", using: :btree
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "city"
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_skills_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,4 +67,8 @@ ActiveRecord::Schema.define(version: 20170213135253) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "bookings", "skills"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "bookings"
+  add_foreign_key "skills", "users"
 end
