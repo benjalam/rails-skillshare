cba04d8486c8503438fa19c23b2e095a2d7e3358
Leslie Maarek <maarek.leslie@gmail.com>
2017-02-14 10:11:39 +0100

    Nesting des reviews dans booking

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :reviews
-  end
+  resources :users
   resources :skills do
-    resources :bookings, only: [:new, :show, :create]
+    resources :bookings, only: [:new, :show, :create] do
+      resources :reviews
+    end
   end
 end
