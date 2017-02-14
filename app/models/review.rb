2f984f85e23c9c1cf3fd50163e2325bd60dd045e
Federico Volonteri <federico.volonteri@gmail.com>
2017-02-14 16:50:08 +0100

    users are now accessibles but edit option only if current user signed in + default skills photo added

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :rating, presence: true
+  validates :nature, inclusion: { in: ["skillman","client"], allow_nil: false }
 end
