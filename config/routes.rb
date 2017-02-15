f100693173adeeedf931d17e80dbff658c8b1c4c
Federico Volonteri <federico.volonteri@gmail.com>
2017-02-15 16:40:35 +0100

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, only: [:show, :edit, :update]
   resources :skills do
     resources :bookings, only: [:new, :show, :create] do
       resources :reviews
