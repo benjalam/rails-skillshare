11f33f43aa3aa4678a46118a34236e3e7382f97e
Leslie Maarek <maarek.leslie@gmail.com>
2017-02-15 12:11:48 +0100

    debut booking dependent destroy de skill

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,6 @@
 class Skill < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
   validates :title, presence: true
   validates :description, presence: true
