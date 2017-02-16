2015942d13cdbe637684a8d7494c244ab950001b
Federico Volonteri <federico.volonteri@gmail.com>
2017-02-16 17:27:23 +0100

    Added Final_price to booking and display on confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216100704) do
+ActiveRecord::Schema.define(version: 20170216161845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(version: 20170216100704) do
     t.string   "status"
     t.integer  "user_id"
     t.integer  "skill_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "final_price"
     t.index ["skill_id"], name: "index_bookings_on_skill_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
